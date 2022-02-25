e2497739aa0ab4af332009316b3a763dcdb3daef
faaaaasih <fasihq38@gmail.com>
2022-02-25 11:27:35 +0000

    created a class called createBlock - can be used as an object

--- Mock Blockchain Network/Block.py
+++ Mock Blockchain Network/Block.py
@@ -1,51 +1,66 @@
 import hashlib
 import json
+from flask import Flask
 
-def hashdata(inputdata):
-    encoded = inputdata.encode()
-    hashvalue = hashlib.sha256(encoded)
-    hex = hashvalue.hexdigest()
-    return hex
 
-"""
-def calculateNonce(inputhexvalue):  #assume the threshold is 0000....
-    nonce = 0 
-    for 
-"""
+#app = Flask(__name__)
 
-def createBlock(BlockNumber, Data):
-    numtostr = str(BlockNumber)
-    dicttostr = json.dumps(Data)
 
-    index = 0
-    nonce = 0
-    hashedvalue = ""
-
-    while (index==0):
-        finaldata = str(numtostr + dicttostr) + str(nonce)
-        hashedvalue = hashdata(finaldata)
-
-        first4hash = hashedvalue[0:4]
-
-        if (first4hash == "0000"):
-            break
-        
-        nonce +=1
+class CreateBlock:
     
-    finalBlock = {
-        "BLOCK NUMBER": BlockNumber,
+    def hashdata(self,inputdata):
+        encoded = inputdata.encode()
+        hashvalue = hashlib.sha256(encoded)
+        hex = hashvalue.hexdigest()
+        return hex
+    
+    def createBlock(self,Blocknumber,data):
+        numtostr = str(Blocknumber)
+        dicttostr = str(data)
+        index =0
+        nonce =0
+
+        hashedvalue = ""
+
+        while (index == 0):
+            finaldata = numtostr + dicttostr + str(nonce)
+            hashedvalue = self.hashdata(finaldata)
+            first4hash = hashedvalue[0:4]
+            nonce+=1
+
+            if (first4hash == "0000"):
+                break
+        
+        
+        finalBlock = {
+        "BLOCK NUMBER": Blocknumber,
         "NONCE": nonce,
-        "DATA TX": Data,
-        "HASH": hashedvalue
-    }
+        "DATA TX": data,
+        "HASH": hashedvalue}
 
-    finalBlockdict = json.dumps(finalBlock, indent=4)
+        finalBlockdict = json.dumps(finalBlock, indent=4)
 
-    return finalBlockdict
+        return finalBlockdict
 
 
-a = createBlock(1,{"TEST": "TEST1", "TEST2": "TEST4"})
+
+block = CreateBlock()
+
+a = block.createBlock(1,
+{"Hello":"MynameisFasih",
+ "StupidIdiot": "GetLost"})
+
+
 
 print (a)
 
-    
+
+"""    
+@app.route('/')
+def blockcreation():
+    a = createBlock(6565656,{"TEST": "TEST1", "TEST2": "TEST4"})
+    return a
+
+
+#print (a)    
+"""
